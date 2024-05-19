32df631c1829544717d3a797ebe1958afa7dc94d
samchan0221 <samchan0221@gmail.com>
2024-05-19 19:58:11 +0800

    add zsh-nix-shell

--- nix/home/zsh.nix
+++ nix/home/zsh.nix
@@ -1,10 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
     initExtra = builtins.readFile ../../.config/zsh/after.zshrc;
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
   };
   programs.zsh.oh-my-zsh = {
     enable = true;
