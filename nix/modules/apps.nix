91cb02d317e8cbfc94cc051113982eb9b96b9b77
samchan0221 <samchan0221@gmail.com>
2024-07-03 01:14:02 +0800

    disable homebrew first

--- nix/modules/apps.nix
+++ nix/modules/apps.nix
@@ -1,21 +1,20 @@
 { pkgs, ... }: {
-  homebrew = {
-    enable = true;
-
-    onActivation = {
-      autoUpdate = false;
-      # 'zap': uninstalls all formulae(and related files) not listed here.
-      cleanup = "zap";
-    };
-
-    taps = [
-      "homebrew/cask-fonts"
-      "homebrew/services"
-      "homebrew/cask-versions"
-    ];
-
-    brews = [
-      "node@14"
-    ];
-  };
+  # homebrew = {
+  #   enable = true;
+  #
+  #   # onActivation = {
+  #   #   autoUpdate = false;
+  #   #   # 'zap': uninstalls all formulae(and related files) not listed here.
+  #   #   cleanup = "zap";
+  #   # };
+  #
+  #   # taps = [
+  #   #   "homebrew/cask-fonts"
+  #   #   "homebrew/services"
+  #   #   "homebrew/cask-versions"
+  #   # ];
+  #
+  #   brews = [
+  #   ];
+  # };
 }
